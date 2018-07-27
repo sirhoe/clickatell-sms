2337fdda82b5ec752dfedc6048e764ca6110305d
sirhoe <sirhoe@gmail.com>
2018-07-27 15:08:21 +1000

    rename test to setup

--- test/setup.js
+++ test/setup.js
@@ -4,14 +4,16 @@ const expect = require('chai').expect;
 const ERRORS = require('../lib/errors');
 const Clickatell = require('../lib/index');
 
-describe('clickatell-sms', function () {
+describe('Initialization and instantiation', async () => {
     it(`should thorw \"${ERRORS.API_REQUIRED}\" errro when instantiated without key`, () => {
         expect(require('../index')).to.throw(ERRORS.API_REQUIRED);
     });
 
     it('should instantiate a new Clickatell Object when required with apiKey', () => {
         const target = require('../index')('key');
-        expect(target).to.be.an.instanceOf(Clickatell);
         expect(target).to.be.a('object');
+        expect(target).to.be.an.instanceOf(Clickatell);
+        expect(target.sendSMS).to.exist;
+        expect(typeof target.sendSMS).to.equal('function');
     });
 });
